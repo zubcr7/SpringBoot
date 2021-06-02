8f5af7d79c612e2de543ca9a913572fbb861f0bc
zubcr7 <59473038+zubcr7@users.noreply.github.com>
2021-06-02 12:28:33 +0530

    Update SpringBootJpaProjectApplication.java

--- SpringBootJpaProject/src/main/java/com/example/SpringBootJpaProjectApplication.java
+++ SpringBootJpaProject/src/main/java/com/example/SpringBootJpaProjectApplication.java
@@ -3,11 +3,23 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
+
 @SpringBootApplication
-public class SpringBootJpaProjectApplication {
+public class SpringBootJpaProjectApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaProjectApplication.class, args);
 	}
+	
+	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootJpaProjectApplication.class)
+		}
 
 }
